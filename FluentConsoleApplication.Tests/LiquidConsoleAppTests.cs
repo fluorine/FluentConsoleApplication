925101d10385e6a26a64b0bcf9a3cbd5da63b705
Fluorine <joseph.anglada92@gmail.com>
2018-03-07 00:40:30 -0400

    Test for for library usage

---
+++ FluentConsoleApplication.Tests/LiquidConsoleAppTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.Collections;
+
+namespace FluentConsoleApplication.Tests
+{
+    [TestFixture]
+    public class LiquidConsoleApplicationTests
+    {
+        [Test]
+        public void LiquidConsoleApp_DefineConsoleApp_TotalOfCalculation()
+        {
+            // ARRANGE
+            var numberA = 5;
+            var numberB = 2;
+
+            int sum = 0;
+            double product = 0;
+            IFluentConsoleApplication app =
+                FluentConsoleApplication.Create("Calculator", "Use this application for math.")
+                  .DefineCommand("add", "Add two numbers")
+                    .WithParameter<int>("X", "First operand", input => int.Parse(input))
+                    .WithParameter<int>("Y", "Second operand", input => int.Parse(input))
+                      .Does(args => sum = args.X + args.Y)
+                  .DefineCommand("mult", "Multiply two numbers")
+                    .WithParameter<double>("X", "First operand", input => double.Parse(input))
+                    .WithParameter<double>("Y", "Second operand", input => double.Parse(input))
+                      .Does(args => product = args.X * args.Y);
+
+            // ACT
+            //   Emulate user input
+            app.Run($"add {numberA} {numberB}");
+            app.Run($"mult {numberA} {numberB}");
+
+            // ASSERT
+            sum.Should().Be(numberA + numberB);
+            product.Should().BeApproximately(numberA * numberB, precision: 0.01);
+        }
+    }
+}
